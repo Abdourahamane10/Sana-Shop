6acde47e1f5bfb99ed9ec517b07dc9a1249e9d06
Abdourahamane <amdroudjae@gmail.com>
2023-01-22 23:02:07 +0100

    Création de la fonction saveProduct dans le contrôleur ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,17 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function addProduct()
     {
-        return view('admin.addProduct');
+        $categories = Category::all()->pluck('category_name', 'category_name');
+        return view('admin.addProduct')->with('categories', $categories);
     }
 
     public function products()
     {
         return view('admin.products');
+    }
+
+    public function saveProduct(Request $request)
+    {
+        $this->validate($request, ['product_name' => 'Required', 'product_price' => 'Required', 'product_category' => 'Required', 'product_image' => 'image|nullable|max:1999']);
+        if ($request->file('product_image')) {
+            //nom du fichier avec extension
+            $fileNameWithExt = $request->file('product_image')->getClientOriginalName();
+            //nom du fichier sans extension
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            //extension du fichier
+            $extension = $request->file('product_image')->getClientOriginalExtension();
+            //nom du fichier dans le store
+            $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+            //upload image
+            $path = $request->file('product_image')->storeAs('public/product_images', $fileNameToStore);
+        } else {
+            $fileNameToStore = 'noImage.jpg';
+        }
+        $product = new Product();
+        $product->product_name = $request->input('product_name');
+        $product->product_price = $request->input('product_price');
+        $product->product_category = $request->input('product_category');
+        $product->product_image = $fileNameToStore;
+        $product->save();
+
+        return back()->with('status', 'Le product ' . $product->product_name . ' a été ajouté avec succès !!');
     }
 }

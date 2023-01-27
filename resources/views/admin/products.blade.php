28db13474f1944aeca814ee56587ae95390726e6
Abdourahamane <amdroudjae@gmail.com>
2023-01-27 23:07:08 +0100

    Modification de la vue resources/views/admin/products.blade.php pour afficher tous les produits

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -32,6 +32,7 @@ Products
     </section>
 
     <!-- Main content -->
+    {{ Form::hidden('', $increment = 1) }}
     <section class="content">
         <div class="container-fluid">
             <div class="row">
@@ -54,16 +55,17 @@ Products
                                     </tr>
                                 </thead>
                                 <tbody>
+                                    @foreach ($products as $product)
                                     <tr>
-                                        <td>1</td>
+                                        <td>{{ $increment }}</td>
                                         <td>
-                                            <img src="backend/dist/img/user2-160x160.jpg"
+                                            <img src="storage/product_images/{{ $product->product_image }}"
                                                 style="height : 50px; width : 50px" class="img-circle elevation-2"
                                                 alt="User Image">
                                         </td>
-                                        <td>Win 95+</td>
-                                        <td> 4</td>
-                                        <td>5</td>
+                                        <td>{{ $product->product_name }}</td>
+                                        <td> {{ $product->product_category }}</td>
+                                        <td> {{'$ '. $product->product_price }}</td>
                                         <td>
                                             <a href="#" class="btn btn-success">Unactivate</a>
                                             <a href="#" class="btn btn-primary"><i class="nav-icon fas fa-edit"></i></a>
@@ -71,23 +73,8 @@ Products
                                                     class="nav-icon fas fa-trash"></i></a>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>
-                                            <img src="backend/dist/img/user2-160x160.jpg"
-                                                style="height : 50px; width : 50px" class="img-circle elevation-2"
-                                                alt="User Image">
-                                        </td>
-                                        <td>Win 95+</td>
-                                        <td>5</td>
-                                        <td>5</td>
-                                        <td>
-                                            <a href="#" class="btn btn-warning">Activate</a>
-                                            <a href="#" class="btn btn-primary"><i class="nav-icon fas fa-edit"></i></a>
-                                            <a href="#" id="delete" class="btn btn-danger"><i
-                                                    class="nav-icon fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
+                                    {{ Form::hidden('', $increment = $increment + 1) }}
+                                    @endforeach
                                 </tbody>
                                 <tfoot>
                                     <tr>

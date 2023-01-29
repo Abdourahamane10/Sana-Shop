be251c26390081b24eb4ede32d53131d3c45030f
Abdourahamane <amdroudjae@gmail.com>
2023-01-29 19:42:21 +0100

    Modification du type de la colonne product_price dans la migration database/migrations/2023_01_22_205803_create_products_table.php

--- database/migrations/2023_01_22_205803_create_products_table.php
+++ database/migrations/2023_01_22_205803_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('product_name');
-            $table->string('product_price');
+            $table->float('product_price');
             $table->string('product_category');
             $table->string('product_image');
             $table->timestamps();

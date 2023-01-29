107193463ea87e618e677fa25091027630d16782
Abdourahamane <amdroudjae@gmail.com>
2023-01-29 19:40:36 +0100

    Définition de la fonction updateProduct du contrôleur ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -56,5 +57,31 @@ class ProductController extends Controller
 
     public function updateProduct(Request $request)
     {
+        $this->validate($request, ['product_name' => 'Required', 'product_price' => 'Required', 'product_category' => 'Required', 'product_image' => 'image|nullable|max:1999']);
+        $product = Product::find($request->input('id'));
+        $product->product_name = $request->input('product_name');
+        $product->product_price = $request->input('product_price');
+        $product->product_category = $request->input('product_category');
+
+        if ($request->hasFile('product_image')) {
+            //nom du fichier avec extension
+            $fileNameWithExt = $request->file('product_image')->getClientOriginalName();
+            //nom du fichier sans extension
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            //extension du fichier
+            $extension = $request->file('product_image')->getClientOriginalExtension();
+            //nom du fichier dans le store
+            $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+            //upload image
+            $path = $request->file('product_image')->storeAs('public/product_images', $fileNameToStore);
+            //On vérifie si l'image du produit n'est pas l'image par defaut et on la supprime dans le storage
+            if ($product->product_image != 'noImage.jpg') {
+                Storage::delete('public/product_images/' . $product->product_image);
+            }
+            $product->product_image = $fileNameToStore;
+        }
+
+        $product->update();
+        return redirect('/products')->with('status', "Le produit " . $product->product_name . " a été mis à jour avec succès !!");
     }
 }

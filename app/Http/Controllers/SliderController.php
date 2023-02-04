207ed3da3a3bd729288f1dec3bd8e20ec5b2d03d
Abdourahamane <amdroudjae@gmail.com>
2023-02-04 01:19:02 +0100

    Récupération de la liste des sliders dans la fonction sliders du contrôleur SliderController et création de la fonction saveSlider dans le contrôleur SliderController

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class SliderController extends Controller
@@ -13,6 +14,31 @@ class SliderController extends Controller
 
     public function sliders()
     {
-        return view('admin.sliders');
+        $sliders = Slider::all();
+        return view('admin.sliders')->with('sliders', $sliders);
+    }
+
+    public function saveSlider(Request $request)
+    {
+        $this->validate($request, ['description1' => 'Required', 'description2' => 'Required', 'slider_image' => 'image|required|max:1999']);
+
+        //nom du fichier avec extension
+        $fileNameWithExt = $request->file('slider_image')->getClientOriginalName();
+        //nom du fichier sans extension
+        $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+        //extension du fichier
+        $extension = $request->file('slider_image')->getClientOriginalExtension();
+        //nom du fichier dans le store
+        $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+        //upload image
+        $path = $request->file('slider_image')->storeAs('public/slider_images', $fileNameToStore);
+
+        $slider = new Slider();
+        $slider->description1 = $request->input('description1');
+        $slider->description2 = $request->input('description2');
+        $slider->slider_image = $fileNameToStore;
+        $slider->status = 1;
+        $slider->save();
+        return back()->with('status', 'Le slider a été enregistré avec succès !!');
     }
 }

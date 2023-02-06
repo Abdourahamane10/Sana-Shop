b4c57d7c6e9640407c65db8a47ce3921b4fafb79
Abdourahamane <amdroudjae@gmail.com>
2023-02-06 23:42:55 +0100

    Création des fonctions editSlider et updateSlider dans le contrôleur SliderController

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SliderController extends Controller
 {
@@ -40,5 +41,38 @@ class SliderController extends Controller
         $slider->status = 1;
         $slider->save();
         return back()->with('status', 'Le slider a été enregistré avec succès !!');
+    }
+
+    public function editSlider($id)
+    {
+        $slider = Slider::find($id);
+        return view('admin.editSlider')->with('slider', $slider);
+    }
+
+    public function updateSlider(Request $request)
+    {
+        $this->validate($request, ['description1' => 'Required', 'description2' => 'Required', 'slider_image' => 'image|nullable|max:1999']);
+        $slider = Slider::find($request->input('id'));
+        $slider->description1 = $request->input('description1');
+        $slider->description2 = $request->input('description2');
+
+        if ($request->hasFile('slider_image')) {
+            //nom du fichier avec extension
+            $fileNameWithExt = $request->file('slider_image')->getClientOriginalName();
+            //nom du fichier sans extension
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            //extension du fichier
+            $extension = $request->file('slider_image')->getClientOriginalExtension();
+            //nom du fichier dans le store
+            $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
+            //upload image
+            $path = $request->file('slider_image')->storeAs('public/slider_images', $fileNameToStore);
+            //On vérifie si l'image du produit n'est pas l'image par defaut et on la supprime dans le storage
+            Storage::delete('public/slider_images/' . $slider->slider_image);
+            $slider->slider_image = $fileNameToStore;
+        }
+
+        $slider->update();
+        return redirect('/sliders')->with('status', "Le slider a été mis à jour avec succès !!");
     }
 }

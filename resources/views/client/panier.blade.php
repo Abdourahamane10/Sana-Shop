86578eb7a2ab4682498d67f6a00f050058cc667d
Abdourahamane <amdroudjae@gmail.com>
2023-02-12 15:58:58 +0100

    Affichage des produits présents dans le panier dans la vue resources/views/client/panier.blade.php

--- resources/views/client/panier.blade.php
+++ resources/views/client/panier.blade.php
@@ -35,58 +35,37 @@ Panier
                             </tr>
                         </thead>
                         <tbody>
+                            @foreach ($products as $product)
                             <tr class="text-center">
                                 <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
 
                                 <td class="image-prod">
-                                    <div class="img" style="background-image:url(frontend/images/product-3.jpg);">
+                                    <div class="img"
+                                        style="background-image:url(/storage/product_images/{{ $product['product_image'] }});">
                                     </div>
                                 </td>
 
                                 <td class="product-name">
-                                    <h3>Bell Pepper</h3>
+                                    <h3>{{ $product['product_name'] }}</h3>
                                     <p>Far far away, behind the word mountains, far from the countries</p>
                                 </td>
 
-                                <td class="price">$4.90</td>
+                                <td class="price">${{ $product['product_price'] }}</td>
                                 <form action="">
                                     <td class="quantity">
                                         <div class="input-group mb-3">
                                             <input type="number" name="quantity"
-                                                class="quantity form-control input-number" value="1" min="1" max="100">
+                                                class="quantity form-control input-number" value="{{ $product['qty'] }}"
+                                                min="1" max="100">
                                         </div>
                                 </form>
 
 
                                 </td>
 
-                                <td class="total">$4.90</td>
+                                <td class="total">${{ $product['product_price']*$product['qty'] }}</td>
                             </tr><!-- END TR-->
-
-                            <tr class="text-center">
-                                <td class="product-remove"><a href="#"><span class="ion-ios-close"></span></a></td>
-
-                                <td class="image-prod">
-                                    <div class="img" style="background-image:url(frontend/images/product-4.jpg);">
-                                    </div>
-                                </td>
-
-                                <td class="product-name">
-                                    <h3>Bell Pepper</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries</p>
-                                </td>
-
-                                <td class="price">$15.70</td>
-
-                                <td class="quantity">
-                                    <div class="input-group mb-3">
-                                        <input type="text" name="quantity" class="quantity form-control input-number"
-                                            value="1" min="1" max="100">
-                                    </div>
-                                </td>
-
-                                <td class="total">$15.70</td>
-                            </tr><!-- END TR-->
+                            @endforeach
                         </tbody>
                     </table>
                 </div>

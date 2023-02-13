c09fdaf0f08fa66ea973b327ac9121ad9a0fa1b4
Abdourahamane <amdroudjae@gmail.com>
2023-02-13 22:52:48 +0100

    Mise en place de l'url de traitement du formulaire d'authentification et affichage de message en cas d'erreur d'authentification

--- resources/views/client/login.blade.php
+++ resources/views/client/login.blade.php
@@ -34,7 +34,8 @@
     <div class="limiter">
         <div class="container-login100" style="background-image: url('images/bg-01.jpg');">
             <div class="wrap-login100">
-                <form class="login100-form validate-form">
+                <form action="{{ url('accederCompte') }}" class="login100-form validate-form" method="POST">
+                    {{ csrf_field() }}
                     <a href="{{ url('/') }}">
                         <span class="login100-form-logo">
                             <i class="zmdi zmdi-landscape"></i>
@@ -45,13 +46,20 @@
                         Log in
                     </span>
 
+                    @if (Session::has('status'))
+                    <div class="alert alert-danger">
+                        {{ Session::get('status') }}
+                        {{ Session::put('status', null) }}
+                    </div>
+                    @endif
+
                     <div class="wrap-input100 validate-input" data-validate="Enter username">
-                        <input class="input100" type="text" name="username" placeholder="Username">
+                        <input class="input100" type="text" name="email" placeholder="Username">
                         <span class="focus-input100" data-placeholder="&#xf207;"></span>
                     </div>
 
                     <div class="wrap-input100 validate-input" data-validate="Enter password">
-                        <input class="input100" type="password" name="pass" placeholder="Password">
+                        <input class="input100" type="password" name="password" placeholder="Password">
                         <span class="focus-input100" data-placeholder="&#xf191;"></span>
                     </div>
 

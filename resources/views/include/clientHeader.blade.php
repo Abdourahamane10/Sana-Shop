efa842b28f947526415954070f8a24cffc556bed
Abdourahamane <amdroudjae@gmail.com>
2023-02-13 22:57:02 +0100

    Dynamisation du header pour afficher Login si on est pas encore connecté sion afficher Logout dans la vue resources/views/include/clientHeader.blade.php

--- resources/views/include/clientHeader.blade.php
+++ resources/views/include/clientHeader.blade.php
@@ -37,7 +37,11 @@
                 <li class="nav-item cta cta-colored"><a href="{{ url('/panier') }}" class="nav-link"><span
                             class="icon-shopping_cart"></span>[{{ Session::has('cart') ? Session::get('cart')->totalQty
                         : 0 }}]</a></li>
+                @if (Session::has('client'))
+                <li class="nav-item active"><a href="{{ url('logout') }}" class="nav-link">Logout</a></li>
+                @else
                 <li class="nav-item active"><a href="{{ url('login') }}" class="nav-link">Login</a></li>
+                @endif
 
             </ul>
         </div>

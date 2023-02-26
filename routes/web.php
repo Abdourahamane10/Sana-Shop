4a92088f5198bf8e340c5bd0657badc1c47f8106
Abdourahamane <amdroudjae@gmail.com>
2023-02-26 18:40:24 +0100

    Création de middleware groupée pour les routes qui concernent l'administration de l'application, Route vers l'url /admin qui appelle la fonction dashboard du contrôleur AdminController commenté et routes vers les urls /admin, /dasboard et /profile decommenté

--- routes/web.php
+++ routes/web.php
@@ -20,25 +20,25 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/admin', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});*/
+});
 
 Route::get('/', [ClientController::class, 'home']);
 Route::get('/shop', [ClientController::class, 'shop']);
 Route::get('/panier', [ClientController::class, 'panier']);
 Route::get('/paiement', [ClientController::class, 'paiement']);
-Route::get('/login', [ClientController::class, 'login']);
+Route::get('/login1', [ClientController::class, 'login']);
 Route::get('/signup', [ClientController::class, 'signup']);
 Route::get('/orders', [ClientController::class, 'orders']);
 Route::get('/ajouterAuPanier/{id}', [ClientController::class, 'ajouterAuPanier']);
@@ -50,39 +50,41 @@ Route::get('/logout', [ClientController::class, 'logout']);
 Route::post('/payer', [ClientController::class, 'payer']);
 
 
-Route::get('/admin', [AdminController::class, 'dashboard']);
+//Route::get('/admin', [AdminController::class, 'dashboard']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/addCategory', [CategoryController::class, 'addCategory']);
+    Route::get('/categories', [CategoryController::class, 'categories']);
+    Route::post('/saveCategory', [CategoryController::class, 'saveCategory']);
+    Route::get('/editCategory/{id}', [CategoryController::class, 'editCategory']);
+    Route::post('/updateCategory', [CategoryController::class, 'updateCategory']);
+    Route::get('/deleteCategory/{id}', [CategoryController::class, 'deleteCategory']);
 
 
-Route::get('/addCategory', [CategoryController::class, 'addCategory']);
-Route::get('/categories', [CategoryController::class, 'categories']);
-Route::post('/saveCategory', [CategoryController::class, 'saveCategory']);
-Route::get('/editCategory/{id}', [CategoryController::class, 'editCategory']);
-Route::post('/updateCategory', [CategoryController::class, 'updateCategory']);
-Route::get('/deleteCategory/{id}', [CategoryController::class, 'deleteCategory']);
+
+    Route::get('/addSlider', [SliderController::class, 'addSlider']);
+    Route::get('/sliders', [SliderController::class, 'sliders']);
+    Route::post('/saveSlider', [SliderController::class, 'saveSlider']);
+    Route::get('/editSlider/{id}', [SliderController::class, 'editSlider']);
+    Route::post('/updateSlider', [SliderController::class, 'updateSlider']);
+    Route::get('/activerSlider/{id}', [SliderController::class, 'activerSlider']);
+    Route::get('/desactiverSlider/{id}', [SliderController::class, 'desactiverSlider']);
+    Route::get('/deleteSlider/{id}', [SliderController::class, 'deleteSlider']);
 
 
-
-Route::get('/addSlider', [SliderController::class, 'addSlider']);
-Route::get('/sliders', [SliderController::class, 'sliders']);
-Route::post('/saveSlider', [SliderController::class, 'saveSlider']);
-Route::get('/editSlider/{id}', [SliderController::class, 'editSlider']);
-Route::post('/updateSlider', [SliderController::class, 'updateSlider']);
-Route::get('/activerSlider/{id}', [SliderController::class, 'activerSlider']);
-Route::get('/desactiverSlider/{id}', [SliderController::class, 'desactiverSlider']);
-Route::get('/deleteSlider/{id}', [SliderController::class, 'deleteSlider']);
-
-
-Route::get('/addProduct', [ProductController::class, 'addProduct']);
-Route::get('/products', [ProductController::class, 'products']);
-Route::post('/saveProduct', [ProductController::class, 'saveProduct']);
-Route::get('/editProduct/{id}', [ProductController::class, 'editProduct']);
-Route::post('/updateProduct', [ProductController::class, 'updateProduct']);
-Route::get('/deleteProduct/{id}', [ProductController::class, 'deleteProduct']);
-Route::get('/activerProduct/{id}', [ProductController::class, 'activerProduct']);
-Route::get('/desactiverProduct/{id}', [ProductController::class, 'desactiverProduct']);
-Route::get('/selectParCat/{category_name}', [ProductController::class, 'selectParCat']);
+    Route::get('/addProduct', [ProductController::class, 'addProduct']);
+    Route::get('/products', [ProductController::class, 'products']);
+    Route::post('/saveProduct', [ProductController::class, 'saveProduct']);
+    Route::get('/editProduct/{id}', [ProductController::class, 'editProduct']);
+    Route::post('/updateProduct', [ProductController::class, 'updateProduct']);
+    Route::get('/deleteProduct/{id}', [ProductController::class, 'deleteProduct']);
+    Route::get('/activerProduct/{id}', [ProductController::class, 'activerProduct']);
+    Route::get('/desactiverProduct/{id}', [ProductController::class, 'desactiverProduct']);
+    Route::get('/selectParCat/{category_name}', [ProductController::class, 'selectParCat']);
 
 
-Route::get('/voirCommandePdf/{id}', [PdfController::class, 'voirPdf']);
+    Route::get('/voirCommandePdf/{id}', [PdfController::class, 'voirPdf']);
+});
 
-//require __DIR__ . '/auth.php';
+
+require __DIR__ . '/auth.php';
